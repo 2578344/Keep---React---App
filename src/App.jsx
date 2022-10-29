46d683bab214132c20a69c8bbd780bc31b051dc2
Michal Golabek <2578344g@student.gla.ac.uk>
2022-10-29 06:41:20 +0100

    changed GenerateNote to anonymous function

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import CreateNote from "./CreateNote";
-import testNotes  from "./TestNotes";
+import testNotes from "./TestNotes";
 import Note from "./Note";
-function GenerateNote(noteItem){
-  return <Note key={noteItem.id} title={noteItem.title} content={noteItem.content} />
-}
+
 
 
 function App() {
   return (
     <div className="Header">
-        <Header />
-        <CreateNote />
-        <div className="note-container">
-          {testNotes.map(GenerateNote)}
-        </div>
-        <Footer />
+      <Header />
+      <CreateNote />
+      <div className="note-container">
+        {testNotes.map(noteItem =>
+          <Note key={noteItem.id} title={noteItem.title} content={noteItem.content} />
+        )}
+      </div>
+      <Footer />
     </div>
   );
 }
