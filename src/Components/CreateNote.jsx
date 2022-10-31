c5e31090c384978a2f9c30d1ab8148cedb25dac5
Michal Golabek <2578344g@student.gla.ac.uk>
2022-10-31 19:34:38 +0000

    Separeted react components

--- src/Components/CreateNote.jsx
+++ src/Components/CreateNote.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+
+
 function CreateNote(){
+
+    
     return (
         <div className="create-note">
             <div className="input-note center">
+                <form>
                 <input placeholder="Create a note..."></input>
+                </form>
             </div>
         </div>
     )
